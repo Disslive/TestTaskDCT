ab01aa8346a2941ec85118fc002703f942b2ac4b
Vladislav Skorniak <skorniakv@gmail.com>
2022-05-28 13:50:18 +0300

    base layout

---
+++ TestTaskDCT/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskDCT.ViewModels.Base;
+
+namespace TestTaskDCT.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string _Title = "Main window - Test task";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private string _Theme = "Switch to dark";
+        public string Theme
+        {
+            get => _Theme;
+            set => Set(ref _Theme, value);
+        }
+
+
+    }
+}
