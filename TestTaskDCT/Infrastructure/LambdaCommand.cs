faebbe19030f3a22ab3174d3967ee02f4b36a77d
Vladislav Skorniak <skorniakv@gmail.com>
2022-05-29 11:57:16 +0300

    Command class

---
+++ TestTaskDCT/Infrastructure/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskDCT.Infrastructure.Base;
+
+namespace TestTaskDCT.Infrastructure
+{
+    class LambdaCommand : Command
+    {
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> _Execute, Func<object, bool> _CanExecute = null)
+        {
+            _Execute = Execute;
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
