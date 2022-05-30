1cdf950ed3f6ce41540ebf8b6fba2291cd7393ad
Vladislav Skorniak <skorniakv@gmail.com>
2022-05-30 13:43:54 +0300

    Light and Dark Themes

--- TestTaskDCT/Views/Windows/MainWindow.xaml.cs
+++ TestTaskDCT/Views/Windows/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ namespace TestTaskDCT
         {
             InitializeComponent();
             SwitchLanguage("en");
+            SwitchStyle("Dark");
         }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
@@ -28,7 +29,14 @@ namespace TestTaskDCT
         {
             SwitchLanguage("ua");
         }
-
+        private void ChooseLightTheme(object sender, RoutedEventArgs e)
+        {
+            SwitchStyle("Light");
+        }
+        private void ChooseDarkTheme(object sender, RoutedEventArgs e)
+        {
+            SwitchStyle("Dark");
+        }
         private void SwitchLanguage(string languageCode)
         {
             ResourceDictionary dictionary = new ResourceDictionary();
@@ -42,6 +50,23 @@ namespace TestTaskDCT
                     break;
                 default:
                     dictionary.Source = new Uri("..\\Lang/EN.xaml", UriKind.Relative);
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dictionary);
+        }
+        private void SwitchStyle(string styleName)
+        {
+            ResourceDictionary dictionary = new ResourceDictionary();
+            switch (styleName)
+            {
+                case "Dark":
+                    dictionary.Source = new Uri("..\\Themes/Dark.xaml", UriKind.Relative);
+                    break;
+                case "Light":
+                    dictionary.Source = new Uri("..\\Themes/Light.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dictionary.Source = new Uri("..\\Theme/Light.xaml", UriKind.Relative);
                     break;
             }
             this.Resources.MergedDictionaries.Add(dictionary);
